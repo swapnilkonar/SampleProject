15e3722d5234b6046ce16da7496249d0aaa9ba1e
Swapnil.Konar <Swapnil.Konar@BSL-BNG-L342.betsol.com>
2020-09-09 16:52:05 +0530

    Added string functionility

--- TestSampleProject/src/HelloWorld.java
+++ TestSampleProject/src/HelloWorld.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello everyone");
+		
+		String name= null;
+		String[] newname;
+		Scanner value = new Scanner(System.in);
+		System.out.println("Enter you name");
+		name=value.nextLine();
+		newname=name.split(" ");
+		for( int i = newname.length-1 ; i>=0; i--)
+		{
+			System.out.print(newname[i]+" ");
+		}
+		
+		
 
 	}
 
